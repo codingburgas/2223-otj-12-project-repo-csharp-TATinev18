6e81ad5a2eff4fab8a5ae367a15d6fe5337eb309
TATinev18 <56866283+TATinev18@users.noreply.github.com>
2023-02-19 20:03:32 +0200

    Add ASP.NET MVC Project file

---
+++ Ticket2GO/Ticket2GO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ticket2GO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
