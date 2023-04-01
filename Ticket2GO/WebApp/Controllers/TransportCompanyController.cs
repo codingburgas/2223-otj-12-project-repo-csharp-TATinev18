489d4afaf75060fda39a09f887c1332257dc4c9e
TATinev18 <56866283+TATinev18@users.noreply.github.com>
2023-04-01 23:01:18 +0300

    Add functionality for the creation of transport companies Add ViewModel folder Close issues (closes #14, closes #39, closes #38)

---
+++ Ticket2GO/WebApp/Controllers/TransportCompanyController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using WebApp.Areas.Identity.Data;
+using WebApp.Data;
+using WebApp.Models;
+using WebApp.ViewModels;
+using System.IO;
+
+namespace WebApp.Controllers
+{
+    [Authorize(Roles = "Company Manager")]
+    public class TransportCompanyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TransportCompanyController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var transportCompanies = await _context.TransportCompanies.ToListAsync();
+            return View(transportCompanies);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateTransportCompanyViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using var memoryStream = new MemoryStream();
+                await viewModel.Logo.CopyToAsync(memoryStream);
+                byte[] logoBytes = memoryStream.ToArray();
+
+                var transportCompany = new TransportCompany
+                {
+                    Name = viewModel.Name,
+                    Logo = logoBytes
+                };
+
+                _context.Add(transportCompany);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewModel);
+        }
+    }
+}
---
+++ Ticket2GO/WebApp/ViewModels/CreateTransportCompanyViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using WebApp.Models;
+
+namespace WebApp.ViewModels
+{
+    public class CreateTransportCompanyViewModel
+    {
+        [Required]
+        [Display(Name = "Company Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Logo")]
+        public IFormFile Logo { get; set; }
+    }
+}
