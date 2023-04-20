1b9cabc5328215d95379833008367d23bb181327
TATinev18 <TATinev18@codingburgas.bg>
2023-04-20 12:40:43 +0300

    Add Unit tests for AdminService and TicketService

---
+++ Ticket2GO/WebApp.UnitTests/AdminServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using WebApp.Areas.Identity.Data;
+using WebApp.Data;
+using WebApp.Services;
+using WebApp.ViewModels;
+
+namespace WebApp.Tests
+{
+    public class AdminServiceTests
+    {
+        private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private AdminService _adminService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+            _context = new ApplicationDbContext(options);
+
+            var userStore = new UserStore<ApplicationUser>(_context);
+            _userManager = new UserManager<ApplicationUser>(userStore, null, null, null, null, null, null, null, null);
+
+            var roleStore = new RoleStore<IdentityRole>(_context);
+            _roleManager = new RoleManager<IdentityRole>(roleStore, null, null, null, null);
+
+            _adminService = new AdminService(_userManager, _roleManager, _context);
+        }
+
+        [Test]
+        public async Task GetUsers_ValidInput_ReturnsUsers()
+        {
+            var user = new ApplicationUser { Email = "test@test.com", UserName = "test@test.com", FirstName = "Test", LastName = "User" };
+            await _userManager.CreateAsync(user);
+
+            var userRoleViewModels = await _adminService.GetUsers();
+
+            Assert.AreEqual(1, userRoleViewModels.Count);
+            var viewModel = userRoleViewModels.First();
+            Assert.AreEqual(user.Id, viewModel.UserId);
+            Assert.AreEqual(user.Email, viewModel.Email);
+            Assert.AreEqual(user.FirstName, viewModel.FirstName);
+            Assert.AreEqual(user.LastName, viewModel.LastName);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Database.EnsureDeleted();
+        }
+    }
+}
---
+++ Ticket2GO/WebApp.UnitTests/TicketServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using WebApp.Data;
+using WebApp.Models;
+using WebApp.Services;
+using WebApp.ViewModels;
+
+namespace WebApp.Tests
+{
+    public class TicketServiceTests
+    {
+        private ApplicationDbContext _context;
+        private TicketService _ticketService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+
+            _context = new ApplicationDbContext(options);
+            _ticketService = new TicketService(_context);
+        }
+
+        [Test]
+        public async Task GetTickets_ValidInput_ReturnsFilteredTickets()
+        {
+            var transportCompany = new TransportCompany { Name = "Test Company", Logo = Encoding.UTF8.GetBytes("test-logo.png") };
+            _context.TransportCompanies.Add(transportCompany);
+
+            var bus = new Bus { Name = "Test Bus", SeatsNumber = 50, TransportCompany = transportCompany };
+            _context.Buses.Add(bus);
+
+            var destination = new Destination
+            {
+                StartingDestination = "A",
+                FinalDestination = "B",
+                Departure = DateTime.Now.AddDays(1),
+                TimeOfArrival = DateTime.Now.AddDays(1).AddHours(4),
+                Price = 30.0m,
+                Bus = bus
+            };
+            _context.Destinations.Add(destination);
+            await _context.SaveChangesAsync();
+
+            var result = await _ticketService.GetTickets(transportCompany.TransportCompanyId.ToString(), "A", "B", null);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Destinations.Count);
+
+            var firstResult = result.Destinations.First();
+            Assert.AreEqual(destination.DestinationId, firstResult.DestinationId);
+            Assert.AreEqual(destination.StartingDestination, firstResult.StartingDestination);
+            Assert.AreEqual(destination.FinalDestination, firstResult.FinalDestination);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Database.EnsureDeleted();
+        }
+    }
+}
